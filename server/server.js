c6d14fa74c2f8a48cb916fc1fcb195c7d241924e
Akhilesh1104 <akhileshgupta2404@gmail.com>
2024-10-26 18:33:37 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -4,14 +4,15 @@ import cors from "cors"
 // *********** All-Routes *************
 import auth from "./routes/auth.route.js";
 import user from "./routes/user.route.js";
+import "dotenv/config";
 // *********** All-Routes *************
 
 import cookieParser from "cookie-parser";
 const port = process.env.PORT || 7000;
 
 const app = express();
+connectDb();
 // Use cors middleware
-app.use(cors());
 
 // app.use(
 //   cors({
@@ -23,6 +24,7 @@ app.use(cors());
 
 //middle wares
 app.use(express.json());
+app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
 // app.use(cookieParser());
 
@@ -34,25 +36,24 @@ app.get("/", (req, res) => {
 app.use("/api/auth/v1", auth);
 app.use("/api/user/v1", user);
 
-// for wrong apis
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found. Please check the URL and try again.",
-  });
-});
+// // for wrong apis
+// app.use((req, res) => {
+//   res.status(404).json({
+//     success: false,
+//     message: "Route not found. Please check the URL and try again.",
+//   });
+// });
 
 // Error handling middleware (optional, for other server errors)
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({
-    success: false,
-    message: "Internal server error.",
-    error: err.message,
-  });
-});
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(500).json({
+//     success: false,
+//     message: "Internal server error.",
+//     error: err.message,
+//   });
+// });
 
 app.listen(port, async () => {
   console.log("Server is running on port 7000");
-  await connectDb();
 });
